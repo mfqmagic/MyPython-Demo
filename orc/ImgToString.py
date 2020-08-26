dbe6609a089605da60dd9996bdc05b16323b0e64
mfqmagic <mfqmagic@gmail.com>
2020-08-26 10:23:50 +0900

    スペース、改行コードを除く

--- orc/ImgToString.py
+++ orc/ImgToString.py
@@ -9,12 +9,15 @@ path = os.path.split(os.path.realpath(__file__))[0]
 image = Image.open(path + '/test.png')
 text = pytesseract.image_to_string(image, lang='jpn')
 
-text = text.replace(' ', '')
-text = text.replace('\n\n', '\n')
-#0
+# スペース、改行コードを除く
+while (' ' in text) or ('\n\n' in text):
+    text = text.replace(' ', '')
+    text = text.replace('\n\n', '\n')
+
+# ⓪ ⇒ 0
 text = text.replace(chr(9450), '0')
 
-#1~20
+# ①~⑳ ⇒ 1~20
 for num in range(9312,9332):
     text = text.replace(chr(num),str(num - 9311))
 
